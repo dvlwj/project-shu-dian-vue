2fedc8751bb742c7fc4c6b9f67e9d376d832578f
dvlwj <dvlimweijiang@gmail.com>
2020-05-24 02:01:18 +0700

    add register page template

---
+++ src/views/Register.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <p>This is Register</p>
+        </v-row>
+    </v-container>
+  </v-app>
+</template>
