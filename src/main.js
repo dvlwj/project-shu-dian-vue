19994565ac3415ea78d55ffb6cdf22f39658c5e9
dvlwj <dvlimweijiang@gmail.com>
2020-05-24 01:27:12 +0700

    add validation from vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
+import { ValidationProvider, ValidationObserver } from 'vee-validate';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -6,6 +9,11 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 new Vue({
   router,
