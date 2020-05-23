c90006f59a25c487e241c64c92429a49c943cbd4
dvlwj <dvlimweijiang@gmail.com>
2020-05-23 15:26:50 +0700

    install vue

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome to {{appName}}</h1>
+</template>
+
+<script>
+
+import { VUE_APP_NAME } from "process.env";
+
+export default {
+  name: 'Login',
+  appName: VUE_APP_NAME,
+};
+
+</script>
