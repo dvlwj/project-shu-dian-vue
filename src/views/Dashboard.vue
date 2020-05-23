2f8a1b929b5c0f9d3fced639d7df9a50455d5227
dvlwj <dvlimweijiang@gmail.com>
2020-05-24 01:52:20 +0700

    add template dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <!-- <h3>Welcome {{store.getters.getUsername}}</h3> -->
+          <p>This is dashboard</p>
+        </v-row>
+    </v-container>
+  </v-app>
+</template>
